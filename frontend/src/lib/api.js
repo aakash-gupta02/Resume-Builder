ed5f6adcf5f798b9dbd2297e7a7cc746d65c0a30
Aakash <editsky597@gmail.com>
2026-02-17 23:03:35 +0530

    deploying frontend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://resume-builder-iivc.onrender.com/api";
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://resume-builder-iivc.onrender.com/api";
+// const API_BASE_URL = "http://localhost:4000/api";
 
 
 const api = axios.create({
