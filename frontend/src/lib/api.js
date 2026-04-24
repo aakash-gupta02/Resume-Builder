dffd5353b9cd9cf282925bae0c4a0eb086751167
Aakash Gupta <editsky597@gmail.com>
2026-04-25 01:35:46 +0530

    Update API base URL to use environment variable

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://resume-builder-iivc.onrender.com/api";
-// const API_BASE_URL = "http://localhost:4000/api";
-
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
